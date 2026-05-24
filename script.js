5dbf4c596da1e31544c8e733f8cade8896a8b283
Ethan <watsgames1@gmail.com>
2026-05-24 16:01:35 +0100

    added dark mode

--- script.js
+++ script.js
@@ -17,3 +17,42 @@ function Play()
 }
 
 
+var modal = document.getElementById("settingsMenu");
+if(modal)
+{
+    var btn = document.getElementById("Settings");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    document.getElementById("saveSettings").onclick = function() {
+        modal.style.display = "none";
+    }
+
+    document.getElementById("darkModeToggle").addEventListener("change", function() {
+        if (this.checked) {
+            document.body.classList.add("dark-mode");
+            localStorage.setItem("darkMode", "enabled");
+        } 
+        else {
+            document.body.classList.remove("dark-mode");
+            localStorage.setItem("darkMode", "disabled");
+        }
+    });
+
+}
+
+if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
+    var toggle = document.getElementById("darkModeToggle");
+    if (toggle) toggle.checked = true;
+}
